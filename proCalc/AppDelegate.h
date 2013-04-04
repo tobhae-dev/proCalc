2e2712ebf875c8af8b172b6fdfed9aebe8bd2fd6
Tobias Haehnel <tobias.haehnel@gmail.com>
2013-04-04 23:21:04 +0200

    Initial Commit

---
+++ proCalc/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  proCalc
+//
+//  Created by Tobias Hähnel on 04.04.13.
+//  Copyright (c) 2013 jonatobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ proCalc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  proCalc
+//
+//  Created by Tobias Hähnel on 04.04.13.
+//  Copyright (c) 2013 jonatobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
